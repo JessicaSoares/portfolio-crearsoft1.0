07e84e6b774cd9cc14c7d30aac3c67000c184ae4
jessicaSoares <jessicassoarez@gmail.com>
2023-06-21 14:37:25 -0300

    Foto de fundo e contato

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         </div>
         <About />
         <Works />
-        <Feedbacks />
+       
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
